4bace49b02509c92463f29f3cd9a3ede468e5e27
Jeremy <jeremyhmcelhone@gmail.com>
2026-03-10 18:29:22 -0700

    store sin values in array

--- print-sine.c
+++ print-sine.c
@@ -3,10 +3,17 @@
 
 int main(void) {
 	double angle = 0;
+	double angle_arr[63] = {0}; 
 
+	int i = 0;
 	while (angle <= 2 * M_PI) {
-		printf("%.2lf\n", sin(angle));
+		angle_arr[i] = sin(angle);
 		angle += 0.1;
+		i++;
+	}
+	
+	for (int i = 0; i < 63; i++) {
+		printf("%.2lf\n", angle_arr[i]);
 	}
 }
 
